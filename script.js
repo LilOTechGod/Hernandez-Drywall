494da541c036f33216482ca30791bf32ad5ea275
unknown <perezoscar360@gmail.com>
2022-12-31 02:32:21 -0600

    added all of my html just need to style the page and get the functionality of the right aside to show up

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-$('.carousel.carousel-slider').carousel({fullWidth: true});
+var contactInfo = document.getElementsByClassName('.contact-info');
+
+window.onload = function(){ 
+    document.getElementById('#login-btn').onclick = function() {
+        contactInfo.classList.add('active');
+    }
+};
+
+window.onload = function(){ 
+    document.getElementById('#close-contact-info').onclick = function() {
+        contactInfo.classList.remove('active');
+    }
+};
+
+
+// function for contact form in contact us section
+function SendMail() {
+    var params = {
+        from_name : document.getElementById("fullname").value,
+        email_id : document.getElementById("email_id").value,
+        message : document.getElementById("message").value
+    }
+    emailjs.send("service_yk9o5y8", "template_gmy1ayl", params).then(function (res) {
+        alert("Success! " + res.status);
+    })
+}
