bc2f875ea06411f2e6c7d9f3510ab52cede650ca
unknown <perezoscar360@gmail.com>
2023-01-04 23:57:03 -0600

    got the right aside to work once click the person icon and started the gallery page.html

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var contactInfo = document.getElementsByClassName('.contact-info');
+var contactInfo = document.getElementsByClassName('contact-info');
+var loginbtn =  document.getElementById('login-btn');
+var closeContactInfo = document.getElementById('close-contact-info');
+console.log(loginbtn, closeContactInfo)
 
-window.onload = function(){ 
-    document.getElementById('#login-btn').onclick = function() {
-        contactInfo.classList.add('active');
+    loginbtn.onclick = function() {
+        contactInfo[0].classList.remove('contact-hide');
+        contactInfo[0].classList.add('contact-info-active');
     }
-};
 
-window.onload = function(){ 
-    document.getElementById('#close-contact-info').onclick = function() {
-        contactInfo.classList.remove('active');
+    closeContactInfo.onclick = function() {
+        contactInfo[0].classList.add('contact-hide');
+        contactInfo[0].classList.remove('contact-info-active');
     }
-};
+    
 
 
 // function for contact form in contact us section
